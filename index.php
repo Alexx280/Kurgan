87c2a99958604f2e0ae4a9d5f8bf41e806248bb2
Alexx280 <Alexx280@yandex.ru>
2016-04-29 17:57:55 +0600

    Базовые файлы

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset='UTF-8'>
+    <title> Песок, навоз, опил, дрова </title>
+    <link rel='stylesheet' type='text/css' href='design.css'/>
+    <link rel='stylesheet' type='text/css' href='animate.css'/>
+    <script type="text/javascript" src="jquery.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<h3 id="inner">Песок,   Навоз,   Опил,   Дрова</h3>
+<div id="main">
+    В любое удобное для вас время привезём песок, навоз, опил, дрова.
+    Доставка осуществляется 
+</div>
+
+</body>
+</html>
